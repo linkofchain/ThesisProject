6f55818a646ed44457610f86f458a6d75071fdf6
Peter Cady <pccady@gmail.com>
2025-07-30 11:15:57 +0200

    paper plus tts

--- setup.sh
+++ setup.sh
@@ -1,2 +1,17 @@
-source venv/bin/activate
-echo "virtual environment activated!"
+#!/bin/bash
+
+# Check if .venv already exists
+if [ ! -d ".venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv .venv
+else
+    echo "Virtual environment already exists."
+fi
+
+echo "Activating virtual environment..."
+source .venv/bin/activate
+
+#echo "Installing dependencies..."
+#pip install -r requirements.txt
+
+echo "Done!"
